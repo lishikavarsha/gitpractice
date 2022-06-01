1867c91872ee34ed9e0b034d43d117c3c5e86a5e
Varshitha <varshitha750@gmail.com>
2022-06-01 12:41:43 +0530

    first commit

---
+++ GITPRRACTICCE/src/test/java/com/gitpractice/practice/practice1Test.java
@@ -0,0 +1,15 @@
+package com.gitpractice.practice;
+
+import org.testng.annotations.Test;
+
+public class practice1Test {
+	@Test
+	
+	public void practice()
+	{
+
+	System.out.println("practice1");
+	System.out.println("practice2");
+	System.out.println("practice3");
+	}
+}
